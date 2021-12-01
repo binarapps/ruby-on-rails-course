38f61e6a0c08715e721d0d55d7a2e50a69af23d8
Kamil Filipowski <k.filipowski@binarapps.com>
2021-12-01 09:18:09 +0100

    mvc-models homework

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_105405) do
+ActiveRecord::Schema.define(version: 2021_12_01_065119) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -27,5 +37,7 @@ ActiveRecord::Schema.define(version: 2021_11_05_105405) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
