1e397aec44443f4a759fc0e605fbd6e1bae03280
Piotr Aleksandrowicz <p.aleksandrowicz@binarapps.com>
2021-12-20 11:33:24 +0100

    lesson4 - Controlles Routing Homework

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
