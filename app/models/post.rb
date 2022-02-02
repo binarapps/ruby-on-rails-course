8e89aa88a96b91aa4e3065dc7295867d0eb155ba
Kamil Filipowski <k.filipowski@binarapps.com>
2022-02-02 14:18:24 +0100

    lesson-8-code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, :body, presence: true
+
+  def capitalized_title
+    title.capitalize
+  end
+
+  def self.titles
+    Post.all.pluck(:title)
+  end
 end
